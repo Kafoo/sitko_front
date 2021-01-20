d1356cefbf6ffa806d38affb160ba56550b2d7a5
Antoine <ant.guillard@gmail.com>
2021-01-20 23:03:18 +0100

    minupdate

--- src/ts/router/index.ts
+++ src/ts/router/index.ts
@@ -159,6 +159,7 @@ const routes = [
   {
     path: "/account",
     name: "Account",
+    beforeEnter: auth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
