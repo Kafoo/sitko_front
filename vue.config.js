fa28a6d9ee0ece83d49c0884e604d92fbc49e0de
Antoine <ant.guillard@gmail.com>
2021-06-18 15:38:03 +0200

    minupdate

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 
-  publicPath: '/frontend/dist/',
+  publicPath: '/home/u790612287/domains/lesiteco.com/public_html/frontend/dist/',
 
   devServer: {
     host: "localhost"
